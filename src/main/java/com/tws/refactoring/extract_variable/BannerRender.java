8d239b10fada4313da294a8283a20ff6dd5a60f4
xiexinfang <1036554353@qq.com>
2019-08-27 15:12:21 +0800

    bannerRender Test

--- src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
+++ src/main/java/com/tws/refactoring/extract_variable/BannerRender.java
@@ -3,7 +3,7 @@ package com.tws.refactoring.extract_variable;
 public class BannerRender {
 	private String platform;
 	private String browser;
-    String renderBanner(String platform, String browser) {
+    public String renderBanner(String platform, String browser) {
         if ((platform.toUpperCase().indexOf("MAC") > -1) &&
                 (browser.toUpperCase().indexOf("IE") > -1)) {
             return "IE on Mac?";
--- src/test/java/com/tws/refactoring/extract_method/BannerRenderTest.java
+++ src/test/java/com/tws/refactoring/extract_method/BannerRenderTest.java
@@ -4,6 +4,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.tws.refactoring.extract_variable.BannerRender;
+
+import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -15,6 +18,12 @@ public class BannerRenderTest {
 public void should_return_banner_when_given_IEonMAC() {
 	String platform="MAC";
 	String browser="IE";
+	String result;
+	BannerRender bannerRender = new BannerRender();
+	
+	result = bannerRender.renderBanner(platform,browser);
+	
+	assertEquals("IE on Mac?", result);
 	
 }
 
