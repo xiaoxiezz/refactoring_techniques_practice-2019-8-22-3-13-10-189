c006f8d13f568f8a947c46003513f935f9e84566
xiexinfang <1036554353@qq.com>
2019-08-27 15:07:00 +0800

    PriceCaculator done

--- src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
+++ src/main/java/com/tws/refactoring/extract_variable/PriceCalculator.java
@@ -4,10 +4,14 @@ public class PriceCalculator {
 	
    public double getPrice(int quantity, int itemPrice) {
         // Price consists of: base price - discount + shipping cost
-    	
+    	double discountStartPrice=500.0;
+    	double discountRate = 0.05;
+    	double postRate = 0.1;
+    	double maxPostMoney=100.0;
+	   
     	double basePrice=quantity * itemPrice;
-    	double discount=Math.max(0, quantity - 500.0) * itemPrice * 0.05;
-    	double shippingCost=Math.min(quantity * itemPrice * 0.1, 100.0);
+    	double discount=Math.max(0, quantity - discountStartPrice) * itemPrice * discountRate;
+    	double shippingCost=Math.min(quantity * itemPrice * postRate, maxPostMoney);
     	double result=basePrice-discount+shippingCost;
         
     	return result;
